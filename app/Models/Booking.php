5c383637f6d84d9d1084026619d884883aef08bc
MBABAZI Jacques <71966425+dream-hun@users.noreply.github.com>
2025-02-27 09:28:47 +0200

    wip

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -6,6 +6,7 @@ use Carbon\Carbon;
 use DateTimeInterface;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Booking extends Model
 {
@@ -56,5 +57,13 @@ class Booking extends Model
     public function setDateAndTimeAttribute($value)
     {
         $this->attributes['date_and_time'] = $value ? Carbon::createFromFormat(config('panel.date_format').' '.config('panel.time_format'), $value)->format('Y-m-d H:i:s') : null;
+    }
+
+    public static function boot()
+    {
+        parent::boot();
+        static::creating(function ($model) {
+            $model->uuid = (string) Str::uuid();
+        });
     }
 }
